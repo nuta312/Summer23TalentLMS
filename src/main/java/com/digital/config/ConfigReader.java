8be9b97babba67878ee43ea38187330c7defd470
Aibike <aibikekimanbaeva@gmail.com>
2023-10-18 10:34:49 +0600

    added files

---
+++ src/main/java/com/digital/config/ConfigReader.java
@@ -0,0 +1,33 @@
+package com.digital.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+	/**
+	 * Загружает конфигурационные данные из файла config.properties.
+	 */
+	private static Properties properties;
+	static { // статичный блок загружается при загрузке класса и инициал поля
+		try {
+			String path ="C:\\Users\\Asus\\IdeaProjects\\Project-UI\\UI\\src\\main\\resources\\config.properties";
+			FileInputStream inputStream = new FileInputStream(path);
+			// для чтения содержимого файла по указанному пути
+			properties = new Properties();
+			// объект этого класса
+			properties.load(inputStream);
+			// загружает в этот класс все данные
+			inputStream.close();
+			// закрыли поток
+		}catch(IOException e){
+			e.printStackTrace();
+			// распечатали ошибку в случае чего
+		}
+	}
+
+	public static String getProperty(String key){
+		return properties.getProperty(key).trim();//возвращает ключ
+	}
+
+}
