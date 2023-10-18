5e0d41f3a6d80a92f69efcd562ed630229692996
Aibike <aibikekimanbaeva@gmail.com>
2023-10-18 22:42:09 +0600

    fix:fix test pack,create new pack for user category pages test

--- src/main/java/com/digital/pages/usersPage/CreateUser.java
+++ src/main/java/com/digital/pages/usersPage/CreateUser.java
@@ -1,7 +1,35 @@
 package com.digital.pages.usersPage;
 
 import com.digital.pages.BasePage;
+import org.openqa.selenium.WebElement;
 
 public class CreateUser extends BasePage {
     //aibike
+
+    public WebElement addUserBtn;
+
+    public CreateUser clickAddUserBtn(){
+        elementActions.clickElement(addUserBtn);
+        return this;
+    }
+
+    public CreateUser fillUpFirstNameInput(){
+        return this;
+    }
+    public CreateUser fillUpLastNameInput(){
+        return this;
+    }
+    public CreateUser fillUpEmailInput(){
+        return this;
+    }
+    public CreateUser fillUpUsernamenput(){
+        return this;
+    }
+    public CreateUser fillUpPasswordInput(){
+        return this;
+    }
+    public CreateUser fillUpBioInput(){
+        return this;
+    }
+
 }
