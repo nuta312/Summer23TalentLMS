c6514fef629deae284a318ff633804b9ce1fd5b1
Aibike <aibikekimanbaeva@gmail.com>
2023-10-18 20:38:05 +0600

    feat:create homepage test and homepage in pages pack

---
+++ src/test/java/com/digital/HomePageTest.java
@@ -0,0 +1,12 @@
+package com.digital;
+
+
+import org.testng.annotations.Test;
+
+public class HomePageTest extends BaseTest {
+    @Test
+    void openUserCategory (){
+        homePage.openUserCategory();
+
+    }
+}
