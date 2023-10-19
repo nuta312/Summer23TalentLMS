fb13adb41f39ae404bc8b5f8f11556e81ba1ee9f
Tahir <tafik13.01.90@gmail.com>
2023-10-19 09:46:01 +0600

    Taha

---
+++ src/main/java/com/digital/pages/coursesPage/AddCategoryPage.java
@@ -0,0 +1,13 @@
+package com.digital.pages.coursesPage;
+
+import com.digital.pages.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AddCategoryPage extends BasePage {
+
+    @FindBy(xpath = "//input[name='name']")
+    public WebElement fieldNameInput;
+
+
+}
