d3f0e905077b15f13bb0de13d12742011c3c36ee
Aibike <aibikekimanbaeva@gmail.com>
2023-10-22 19:04:09 +0600

    fix:clean usertable test file

--- src/test/java/com/digital/userCategoryTest/UserTableTest.java
+++ src/test/java/com/digital/userCategoryTest/UserTableTest.java
@@ -7,33 +7,41 @@ import java.util.*;
 
 public class UserTableTest extends UserHomePageTest {
 
-    @Test(priority = 1)
-    void checkOpenModal() {
-        userTable.clickToDeleteBtn();
-        Assert.assertTrue(userTable.deleteModal.isEnabled());
-    }
+//    @Test(priority = 1)
+//    void checkOpenModal() {
+//        userTable.clickToDeleteBtn();
+//        Assert.assertTrue(userTable.deleteModal.isEnabled());
+//    }
+//
+//    @Test(priority = 2)
+//    void checkTextInModal() {
+//        Assert.assertTrue(userTable.modalHeaderTitle.getText().contains("Delete User?"));
+//        Assert.assertTrue(userTable.modalBodyText.getText().contains("Are you sure you want to delete the user "));
+//        Assert.assertTrue(userTable.deleteModalBtn.getText().contains("Delete"));
+//        Assert.assertTrue(userTable.cancelModalBtn.getText().contains("Cancel"));
+//    }
+//
+//    @Test(priority = 3, description = "this test checks if the user is deleted from the table or if the delete button works.")
+//    void checkForDeleteUserToTable() {
+//        userTable.deleteModalBtn.click();
+//        String expectedString = userTable.userFirstName;
+//        boolean isContained = false;
+//        for (String user : userTable.getAllUsersInTable()) {
+//            if (user.toLowerCase().contains(expectedString)) {
+//                isContained = true;
+//                break;
+//            }
+//        }
+//        Assert.assertFalse(isContained);
+//    }
+//
 
-    @Test(priority = 2)
-    void checkTextInModal() {
-        Assert.assertTrue(userTable.modalHeaderTitle.getText().contains("Delete User?"));
-        Assert.assertTrue(userTable.modalBodyText.getText().contains("Are you sure you want to delete the user "));
-        Assert.assertTrue(userTable.deleteModalBtn.getText().contains("Delete"));
-        Assert.assertTrue(userTable.cancelModalBtn.getText().contains("Cancel"));
-    }
 
-    @Test(priority = 3, description = "this test checks if the user is deleted from the table or if the delete button works.")
-    void checkForDeleteUserToTable() {
-        userTable.deleteModalBtn.click();
-        String expectedString = userTable.userFirstName;
-        boolean isContained = false;
-        for (String user : userTable.getAllUsersInTable()) {
-            if (user.toLowerCase().contains(expectedString)) {
-                isContained = true;
-                break;
-            }
-        }
-        Assert.assertFalse(isContained);
-    }
+
+/*
+Код выше проверяет на удаление пользователя с таблицы если раскомментируете будет удалять при каждом запуске
+ */
+
 
 
     @Test(priority = 4, description = "This test checks if user data is sorted by email after clicking on the email tab.")
@@ -44,12 +52,10 @@ public class UserTableTest extends UserHomePageTest {
         Assert.assertEquals(expectedSortedList, userTable.listOfUserEmailText);
     }
 
-    @Test
+    @Test(priority = 5, description = "This test checks if user data is sorted by user type after clicking on the usertype tab.")
     void testUserTypeSorting() {
         userTable.clickToUserTypeTab();
         HashSet<String> expectedOrder = new HashSet<>(userTable.listOfUserTypeText);
-        System.out.println(expectedOrder);
-        System.out.println(userTable.listOfUserTypeText);
         Assert.assertEquals(expectedOrder, userTable.listOfUserTypeText, "The list is not sorted correctly");
     }
 
