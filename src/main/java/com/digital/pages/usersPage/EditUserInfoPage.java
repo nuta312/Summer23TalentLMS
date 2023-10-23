d0dd5fe167aba8bd6704fe02f60937b1d31f1a12
Ruslan <143115412+Ruslan-NOMAD@users.noreply.github.com>
2023-10-23 21:48:41 +0600

    aaa

--- src/main/java/com/digital/pages/usersPage/EditUserInfoPage.java
+++ src/main/java/com/digital/pages/usersPage/EditUserInfoPage.java
@@ -3,8 +3,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import java.util.List;
-import java.util.NoSuchElementException;
-
 import static com.digital.driver.Driver.getDriver;
 
 public class EditUserInfoPage extends UserHomePage {
