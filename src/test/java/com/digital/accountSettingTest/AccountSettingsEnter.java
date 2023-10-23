e8631269621b873c76b7061044d6607b89c71da3
Aykerim <aika_sony@mail.ru>
2023-10-24 00:33:50 +0600

    feature:add basic settings page

--- src/test/java/com/digital/accountSettingTest/AccountSettingsEnter.java
+++ src/test/java/com/digital/accountSettingTest/AccountSettingsEnter.java
@@ -2,14 +2,17 @@ package com.digital.accountSettingTest;
 
 import com.digital.BaseTest;
 import com.digital.pages.accountSettingsPage.AccountSettingsPage;
+import com.digital.pages.accountSettingsPage.BasicSettings;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class AccountSettingsEnter extends BaseTest {
+    public BasicSettings basicSettings;
 
-    @Test
-    public void BasicSettingsTest () {
+    @BeforeClass
+    public void BasicSettingsTest() {
         accountSettingsPage = new AccountSettingsPage();
         accountSettingsPage.enterAccountSettingsPage();
     }
---
+++ src/test/java/com/digital/accountSettingTest/BasicSettingsTest.java
@@ -0,0 +1,57 @@
+package com.digital.accountSettingTest;
+
+import com.digital.helper.ElementActions;
+import com.digital.pages.accountSettingsPage.BasicSettings;
+import com.fasterxml.jackson.core.JsonParser;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BasicSettingsTest extends AccountSettingsEnter {
+    @Test(priority = 1)
+    void fillUpTheSiteTest() {
+        BasicSettings basicSettings = new BasicSettings();
+        ElementActions elementActions = new ElementActions();
+        basicSettings.fillUpTheSiteName("");
+        basicSettings.fillUpTheSiteDescription("Basic Settings");
+        basicSettings
+                .searchSelectLanguage()
+                .scrollDown()
+                .searchSelectTimeZone()
+                .scrollDown()
+                .internalAnnouncementInput()
+                .displayIconText(basicSettings.internalAnnouncementTool)
+                .externalAnnouncementInput()
+                .displayIconText(basicSettings.externalAnnouncementInfo)
+                .checkBoxWork(basicSettings.checkboxEnableApi)
+                .refreshApiKey()
+                .checkBoxWork(basicSettings.checkboxEnableXApi)
+                .checkBoxWork(basicSettings.checkboxExternalLrs);
+        elementActions.writeText(basicSettings.inputXApiEndpoint, "https://www.google.com/")
+                .writeText(basicSettings.inputXApiKey, "sdfghjk45632")
+                .writeText(basicSettings.inputXApiSecret, "dog");
+        basicSettings.checkBoxWork(basicSettings.checkboxRegistration)
+                .inputDomainRegistration()
+                .checkBoxWork(basicSettings.checkboxDisallowMultipleLogins)
+                .checkBoxWork(basicSettings.checkboxDisallowMobileApp)
+                .checkBoxWork(basicSettings.checkboxIntercomIdentityVerification);
+        elementActions.writeText(basicSettings.inputIntercomSecretKey, "key");
+                basicSettings.checkBoxWork(basicSettings.checkboxDisableVideoTranscoding)
+                .checkBoxWork(basicSettings.checkboxVideoWatermark)
+                .checkBoxWork(basicSettings.checkboxUseDocumentsViewer)
+                .checkBoxWork(basicSettings.checkboxLoadInFrame)
+                .checkBoxWork(basicSettings.checkboxLockSystem)
+                .inputMessage()
+                .checkBoxWork(basicSettings.checkboxHideFromSearchEngines)
+                .checkBoxWork(basicSettings.checkboxExternalCatalog)
+                .checkBoxWork(basicSettings.checkboxShowBranchCourses)
+                .checkBoxWork(basicSettings.checkboxShowCourseInfoPage)
+                .checkBoxWork(basicSettings.checkboxBambooHR)
+                .checkBoxWork(basicSettings.checkboxInactiveUsers);
+        elementActions.writeText(basicSettings.inputSubdomain, "nbu111");
+        basicSettings.clickSave();
+
+    }
+
+}
