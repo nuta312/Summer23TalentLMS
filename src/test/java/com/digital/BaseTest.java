fe5d427767ce3a0e72707ba978cf5b129af7813a
Meervan <meervan.datkaeva.ch@gmail.com>
2023-10-23 20:09:42 +0600

    I added a new AccountHomePageTest class

--- src/test/java/com/digital/BaseTest.java
+++ src/test/java/com/digital/BaseTest.java
@@ -2,13 +2,16 @@ package com.digital;
 
 import com.digital.config.ConfigReader;
 import com.digital.driver.Driver;
+import com.digital.helper.ElementActions;
 import com.digital.pages.HomePage;
 import com.digital.pages.LoginPage;
+import com.digital.pages.accountSettingsPage.AccountHomePage;
 import com.digital.pages.usersPage.UserHomePage;
 import com.digital.pages.accountSettingsPage.AccountSettingsPage;
 import com.digital.pages.accountSettingsPage.CertificatesPage;
 import com.digital.utils.FakeDataProvider;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import com.digital.pages.coursesPage.CategoriesPage;
 
@@ -21,6 +24,8 @@ public abstract class BaseTest {
     public HomePage homePage;
     public FakeDataProvider fakeDataProvider;
     public CategoriesPage categoriesPage;
+    public AccountHomePage accountHomePage;
+    public ElementActions elementActions;
 
 
     @BeforeClass(alwaysRun = true)
@@ -32,6 +37,9 @@ public abstract class BaseTest {
         homePage = new HomePage();
         fakeDataProvider = new FakeDataProvider();
         categoriesPage = new CategoriesPage();
+        accountHomePage= new AccountHomePage();
+        elementActions = new ElementActions();
+
     }
 
 //    @AfterClass(alwaysRun = true)
