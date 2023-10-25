214e8b470f9e94b6e351a5fcacdcb13c7a2fb9bd
Nur <jeldenovanur@gmail.com>
2023-10-25 20:04:58 +0600

    themes

---
+++ src/test/java/com/digital/SauceLabs.java
@@ -0,0 +1,29 @@
+package com.digital;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariOptions;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SauceLabs {
+
+    @Test
+    void test123() throws MalformedURLException {
+        SafariOptions browserOptions = new SafariOptions();
+        browserOptions.setPlatformName("macOS 13");
+        browserOptions.setBrowserVersion("latest");
+        Map<String, Object> sauceOptions = new HashMap<>();
+        sauceOptions.put("username", "oauth-jeldenovanur-149db");
+        sauceOptions.put("accessKey", "*****37d5");
+        sauceOptions.put("build", "<your build id>");
+        sauceOptions.put("name", "<your test name>");
+        browserOptions.setCapability("sauce:options", sauceOptions);
+
+        URL url = new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
+        RemoteWebDriver driver = new RemoteWebDriver(url, browserOptions);
+    }
+}
