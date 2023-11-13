446846f738836041a088560e9d21bd407d03f8b7
Aibike <aibikekimanbaeva@gmail.com>
2023-11-13 21:38:56 +0600

    fix:fixed update method

--- src/main/java/com/digital/db/Tst.java
+++ src/main/java/com/digital/db/Tst.java
@@ -1,12 +1,35 @@
 package com.digital.db;
 
+import com.digital.db.beans.EmployeeBean;
 import com.digital.db.db_utils.DBConnection;
+import org.testng.Assert;
 
 import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Tst {
     public static void main(String[] args) throws SQLException {
         DBConnection.open("employees");
+//        int expectedIndex = 1;
+//        int actualIndex =  EmployeeBean.getByNameLastName("Aibike","Imanbaeva").getEmp_no();
+//        System.out.println(actualIndex);
+//        Assert.assertEquals(expectedIndex,actualIndex);
+//        System.out.println(EmployeeBean.getBy("emp_no", "1"));
+
+//        EmployeeBean denis  = new EmployeeBean(5, "2000-07-15", "denis", "doe", "m", "2022-02-02");
+//        try {
+//            EmployeeBean result = EmployeeBean.createNewEmployee(denis, 5);
+//            System.out.println(result);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+        Map<String, Object> updates = new LinkedHashMap<>();
+        updates.put("first_name", "denis");
+        updates.put("last_name", "totosheva");
+       EmployeeBean res =  EmployeeBean.updateEmployee(2, updates);
+        System.out.println(res);
         DBConnection.close();
 
     }
